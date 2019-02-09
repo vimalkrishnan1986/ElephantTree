f52f5d988cc9956db761152b37b14985d8e359ac
vimalkrishnan <vimal_viyookaran@yahoo.co.in>
2019-02-09 14:56:03 +0530

    BaseController added

---
+++ Easy Billing/Easy Billing/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBilling.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+
+    }
+}
